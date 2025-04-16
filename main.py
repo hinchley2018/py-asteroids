f52830daf58563a8163a9307acf727dc4d997070
Code Nurturers LLC <mobileapps@codenurturers.com>
2025-04-16 13:01:28 -0500

    fps

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:",  SCREEN_HEIGHT)
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill(SCREEN_BG_COLOR)
@@ -16,6 +18,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = clock.tick(FRAMES_PER_SECOND) / 1000
 if __name__ == "__main__":
     main()
